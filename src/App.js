0ad05d62f94ab3d610510309a8715861b814f770
AleksandrNebesnyi <a.nebesnyi@gmail.com>
2021-12-28 21:34:30 +0200

    fix bag

--- src/App.js
+++ src/App.js
@@ -1,42 +1,41 @@
-import {Component} from 'react';
+import { Component } from 'react';
 import Section from './components/Section/Section';
 import ContactForm from './components/ContactForm/ContactForm';
 import ContactList from './components/ContactList/ContactList';
 import ContactsFilter from './components/ContactsFilter/ContactsFilter';
 
-
 class App extends Component {
-  state ={
+  state = {
     contacts: [
       {
-        id: "c7a273d-8bfr-66gr-wef2-4f4d57ea2d0",
-        name: "Rosie Simpson",
-        number: "459-12-56",
+        id: 'c7a273d-8bfr-66gr-wef2-4f4d57ea2d0',
+        name: 'Rosie Simpson',
+        number: '459-12-56',
       },
       {
-        id: "anctrjd-8bfr-66gr-wef2-4f4d57ea2d0",
-        name: "Hermione Kline",
-        number: "443-89-12",
+        id: 'anctrjd-8bfr-66gr-wef2-4f4d57ea2d0',
+        name: 'Hermione Kline',
+        number: '443-89-12',
       },
       {
-        id: "dkt846a-8bfr-66gr-wef2-4f4d57ea2d0",
-        name: "Eden Clements",
-        number: "645-17-79",
+        id: 'dkt846a-8bfr-66gr-wef2-4f4d57ea2d0',
+        name: 'Eden Clements',
+        number: '645-17-79',
       },
       {
-        id: "hr7y3td-8bfr-66gr-wef2-4f4d57ea2d0",
-        name: "Annie Copeland",
-        number: "227-91-26",
+        id: 'hr7y3td-8bfr-66gr-wef2-4f4d57ea2d0',
+        name: 'Annie Copeland',
+        number: '227-91-26',
       },
     ],
-    filter:"",
-     }
+    filter: '',
+  };
 
-     // Добавляет контакт 
+  // Добавляет контакт
   addContact = newContact => {
     // Проверка на дубликат
-    const duplicateName = this.state.contacts.find(
-      contact => contact.name === newContact.name,
+    const duplicateName = this.state.contacts.sort(
+      contact => contact.name.toLowerCase() === newContact.name.toLowerCase(),
     );
 
     if (duplicateName) {
@@ -49,15 +48,11 @@ class App extends Component {
     }));
   };
 
-
-
   // Следит за полем фильтрации и пишет в стейт
   changeFilter = event => {
     event.preventDefault();
-    this.setState({ filter: event.currentTarget.value });
-    console.log(event.currentTarget.value);
-   
-  
+    this.setState({ filter: event.target.value });
+    console.log(event.target.value);
   };
 
   // Фильтрует и возвращает результат фильтра
@@ -66,13 +61,14 @@ class App extends Component {
     console.log(this.state);
     const normalizedFilter = filter.toLowerCase();
 
-    if (filter !=="") {return contacts.filter(contact =>
-      contact.name.toLowerCase().includes(normalizedFilter),
-    )} else{
-           return contacts;
+    if (filter !== '') {
+      return contacts.filter(contact =>
+        contact.name.toLowerCase().includes(normalizedFilter),
+      );
+    } else {
+      return contacts;
     }
-      };
-
+  };
 
   // Удаляет контакт
   deleteContact = id => {
@@ -80,34 +76,27 @@ class App extends Component {
       contacts: prevState.contacts.filter(contact => contact.id !== id),
     }));
   };
-  
 
-  render ()
-
-
-  {
+  render() {
     return (
       <>
-      <Section title="Phonebook">
-      <ContactForm onSubmit={this.addContact} />
-      </Section>
+        <Section title="Phonebook">
+          <ContactForm onSubmit={this.addContact} />
+        </Section>
 
-      <Section title="Contacts" >
-      <ContactsFilter filter={this.state.filter}
-       onFilter={this.changeFilter} />
-      <ContactList 
-      contacts={this.filterContacts()}  
-      onDeleteContact={this.deleteContact}/>
-
-    </Section>
-     </>   
-      
-    )
+        <Section title="Contacts">
+          <ContactsFilter
+            filter={this.state.filter}
+            onFilter={this.changeFilter}
+          />
+          <ContactList
+            contacts={this.filterContacts()}
+            onDeleteContact={this.deleteContact}
+          />
+        </Section>
+      </>
+    );
   }
-
 }
-
-
-
 
 export default App;
